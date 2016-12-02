55beb79401bf31a3285bc08cd022eb613f541365
devuxk <devakumar@crayond.com>
2016-12-02 16:19:09 +0530

    body as var

--- components/https/hasura_post.js
+++ components/https/hasura_post.js
@@ -5,19 +5,7 @@ module.exports = function(req,res,type,url,head,body){
 		url: url,
 		method: type,
 		headers: head,
-		json: {
-			"type" : "insert",
-			"args" : {
-				"table" : "tbl_user_profile",
-				"returning": ["id","hasura_userid","unique_id","first_name","last_name","profile_picture","tbl_master_profile_statusunique_id","facebook_profile"],
-				"objects": [
-				{"hasura_userid" : req.body.hasura_userid, "unique_id": req.body.unique_id,"first_name":req.body.first_name,
-				"last_name": req.body.last_name,"profile_picture":req.body.profile_picture,
-				"email":req.body.email,"mobile_no":req.body.mobile_no,"tbl_master_profile_statusunique_id":req.body.tbl_master_profile_statusunique_id,
-				"facebook_profile":req.body.facebook_profile}
-				]
-			}
-		}
+		json: body
 	}, function(error, response, body){
 		if(error) {
 			console.log(error);
--- components/user_profile/create_profile.js
+++ components/user_profile/create_profile.js
@@ -7,10 +7,19 @@ module.exports = function(app){
 		var type = 'POST'
 		var url = 'https://data.oologic14.hasura-app.io/v1/query';
 		var head = {'Content-Type':'application/json','Authorization':'Bearer pchjjclr94ba1xa00qcq6hf7lo4df8td'}
-		var body = {"hasura_userid" : req.body.hasura_userid, "unique_id": req.body.unique_id,"first_name":req.body.first_name,
+		var body = {
+			"type" : "insert",
+			"args" : {
+				"table" : "tbl_user_profile",
+				"returning": ["id","hasura_userid","unique_id","first_name","last_name","profile_picture","tbl_master_profile_statusunique_id","facebook_profile"],
+				"objects": [
+				{"hasura_userid" : req.body.hasura_userid, "unique_id": req.body.unique_id,"first_name":req.body.first_name,
 				"last_name": req.body.last_name,"profile_picture":req.body.profile_picture,
 				"email":req.body.email,"mobile_no":req.body.mobile_no,"tbl_master_profile_statusunique_id":req.body.tbl_master_profile_statusunique_id,
-				"facebook_profile":req.body.facebook_profile} 
+				"facebook_profile":req.body.facebook_profile}
+				]
+			}
+		} 
     require('.././https/hasura_post')(req,res,type,url,head,body);
 	});
 }
