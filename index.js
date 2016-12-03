a80adecaba72a2739207b9e59f8f17ca75b4c390
devuxk <devakumar@crayond.com>
2016-12-03 13:52:26 +0530

    restaurants name change in index file

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var app = express();
 require('./components/root')(app);
 require('./components/user_profile/create_profile')(app);
 require('./components/user_profile/edit_profile')(app);
-require('./components/restaurents/trending_restaurants')(app);
+require('./components/restaurants/trending_restaurants')(app);
 app.listen(4000);
 console.log("Running in port 4000")
