79bf8ae078e85b07174e00c1dc0b93b7a3bbe7cd
devuxk <devakumar@crayond.com>
2016-12-22 17:58:03 +0530

    bookmark done in local

--- components/https/temp_hasura_post.js
+++ components/https/temp_hasura_post.js
@@ -1,6 +1,13 @@
 var request = require('request');
+var bookmark_value = require('./bookmark_value');
+
+if (typeof localStorage === "undefined" || localStorage === null) {
+	var LocalStorage = require('node-localstorage').LocalStorage;
+	localStorage = new LocalStorage('./scratch');
+}
+
+
 module.exports = function(req,res,type,url,head,body){
-	console.log(head)
 	request({
 		url: url,
 		method: type,
@@ -10,30 +17,44 @@ module.exports = function(req,res,type,url,head,body){
 		if(error) {
 			console.log(error);
 		} else {
-			console.log(response.statusCode, body);
 			var injected_response_data = body;
+			console.log("+====================");
+			console.log(injected_response_data,"----------------------------------");
+			var user_unique_id = req.body.unique_id;
+			var hasura_user_id = req.body.unique_id;
+			localStorage.setItem("user_unique_id", user_unique_id);
+			localStorage.setItem("hasura_user_id", hasura_user_id);
 			injected_response_data.forEach( function (arrayItem)
 			{
-			    var x = arrayItem
-			    		x['food_type'] = 'chinese';
-						x['food_discount'] = '10%';
-						x['is_bookmarked'] = false;
-						x['is_sponsered'] = false;
-						x['restaurant_trendscore'] = 'high';
-						x['restaurant_image'] = [{"unique_id":"123","image_url":"http://www.vidteq.com/chennai/jpg/vt/vtieiDOVTTQITCIDVECDP.jpg"},
-											  {"unique_id":"1234","image_url":"http://www.vidteq.com/chennai/jpg/vt/vtieiDOVTTQITCIDVECDP.jpg"},
-											  {"unique_id":"12345","image_url":"http://www.vidteq.com/chennai/jpg/vt/vtieiDOVTTQITCIDVECDP.jpg"}];	
-						x['recommended_by'] = [{"unique_id":"145551151823","nickname":"mockusername","users_hash_tag_csv":"#sushi, #veg","facebook_id":"1","img_url":"http://media.tumblr.com/tumblr_m0rwptJAEz1qm0omn.jpg"},
-																	{"unique_id":"145551151823","nickname":"mockusername","users_hash_tag_csv":"#sushi, #veg","facebook_id":"1","img_url":"http://media.tumblr.com/tumblr_m0rwptJAEz1qm0omn.jpg"},
-																	{"unique_id":"145551151823","nickname":"mockusername","users_hash_tag_csv":"#sushi, #veg","facebook_id":"1","img_url":"http://media.tumblr.com/tumblr_m0rwptJAEz1qm0omn.jpg"},
-																	{"unique_id":"145551151823","nickname":"mockusername","users_hash_tag_csv":"#sushi, #veg","facebook_id":"1","img_url":"http://media.tumblr.com/tumblr_m0rwptJAEz1qm0omn.jpg"},
-																	{"unique_id":"145551151823","nickname":"mockusername","users_hash_tag_csv":"#sushi, #veg","facebook_id":"1","img_url":"http://media.tumblr.com/tumblr_m0rwptJAEz1qm0omn.jpg"},
-																	{"unique_id":"145551151823","nickname":"mockusername","users_hash_tag_csv":"#sushi, #veg","facebook_id":"1","img_url":"http://media.tumblr.com/tumblr_m0rwptJAEz1qm0omn.jpg"}
-																	];																
-			    console.log(x);
+				var x = arrayItem
+				var restaurant_unique_id = x.unique_id;
+				var user_unique_id = localStorage.getItem('user_unique_id');
+				var hasura_user_id = localStorage.getItem('hasura_user_id');
+				bookmark_value(restaurant_unique_id,x,user_unique_id,hasura_user_id);
+				x['food_type'] = 'chinese';
+				x['food_discount'] = '10%';
+				x['is_sponsered'] = false;
+				x['restaurant_trendscore'] = 'high';
+				x['trend_image_url'] = 'http://www.nandos.com/sites/all/themes/nandos/images/our-home-image-1.png';
+				x['restaurant_image'] = [{"unique_id":"123","image_url":"http://www.vidteq.com/chennai/jpg/vt/vtieiDOVTTQITCIDVECDP.jpg"},
+				{"unique_id":"1234","image_url":"http://www.vidteq.com/chennai/jpg/vt/vtieiDOVTTQITCIDVECDP.jpg"},
+				{"unique_id":"12345","image_url":"http://www.vidteq.com/chennai/jpg/vt/vtieiDOVTTQITCIDVECDP.jpg"}];	
+				x['recommended_by'] = [{"unique_id":"145551151823","nickname":"mockusername","users_hash_tag_csv":"#sushi, #veg","facebook_id":"1","img_url":"http://media.tumblr.com/tumblr_m0rwptJAEz1qm0omn.jpg"},
+				{"unique_id":"145551151823","nickname":"mockusername","users_hash_tag_csv":"#sushi, #veg","facebook_id":"1","img_url":"http://media.tumblr.com/tumblr_m0rwptJAEz1qm0omn.jpg"},
+				{"unique_id":"145551151823","nickname":"mockusername","users_hash_tag_csv":"#sushi, #veg","facebook_id":"1","img_url":"http://media.tumblr.com/tumblr_m0rwptJAEz1qm0omn.jpg"},
+				{"unique_id":"145551151823","nickname":"mockusername","users_hash_tag_csv":"#sushi, #veg","facebook_id":"1","img_url":"http://media.tumblr.com/tumblr_m0rwptJAEz1qm0omn.jpg"},
+				{"unique_id":"145551151823","nickname":"mockusername","users_hash_tag_csv":"#sushi, #veg","facebook_id":"1","img_url":"http://media.tumblr.com/tumblr_m0rwptJAEz1qm0omn.jpg"},
+				{"unique_id":"145551151823","nickname":"mockusername","users_hash_tag_csv":"#sushi, #veg","facebook_id":"1","img_url":"http://media.tumblr.com/tumblr_m0rwptJAEz1qm0omn.jpg"}
+				];
 			});
 			console.log(injected_response_data)
-			res.send(response.statusCode,injected_response_data)
+  			setTimeout(function() {
+				    res.send(response.statusCode,injected_response_data);
+				    console.log('after response','---------------------------------------');
+				    return true;
+				}, 3000);
+			
+
 		}
 	});
 }
--- components/sponsor/unique_generator.js
+++ components/sponsor/unique_generator.js
@@ -2,34 +2,31 @@ var request = require('request');
 var jp = require('jsonpath');
 var shortid = require('shortid');
 if (typeof localStorage === "undefined" || localStorage === null) {
-  var LocalStorage = require('node-localstorage').LocalStorage;
-  localStorage = new LocalStorage('./scratch');
+	var LocalStorage = require('node-localstorage').LocalStorage;
+	localStorage = new LocalStorage('./scratch');
 }
 
 function inside_condition_method(condition_value,unique_id_value){
 	if(condition_value){
-		console.log('callbck the same function')
 		return '65454'
 	} else{
-		console.log('returns unique_id_value')
 		return unique_id_value;
 	}
 }
 
 function condtion_method(error, response, body){
 	if(error) {
-		console.log(error);
 	} else {
 		var array_data = jp.query(response.body, '$..ticket_code');
 		function makeid()
 		{
-		    var text = "";
-		    var possible = "ABCDEFGHJKMNPQRSTUVWXYZ23456789";
+			var text = "";
+			var possible = "ABCDEFGHJKMNPQRSTUVWXYZ23456789";
 
-		    for( var i=0; i < 9; i++ )
-		        text += possible.charAt(Math.floor(Math.random() * possible.length));
+			for( var i=0; i < 9; i++ )
+				text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-		    return text;
+			return text;
 		}
 
 		var unique_id_value =  makeid();
@@ -38,7 +35,7 @@ function condtion_method(error, response, body){
 		}
 		var condition_value = isInArray(unique_id_value, array_data);
 		var var_inside_condition_method = inside_condition_method(condition_value,unique_id_value);
-        return var_inside_condition_method;
+		return var_inside_condition_method;
 	}
 }
 
@@ -56,11 +53,7 @@ module.exports = function(app,hasura_userid,callback){
 		}
 	}, function(error, response, body){
 		var var_condtion_method = condtion_method(error, response, body);
-		console.log(var_condtion_method);
-		console.log('----------');
 		localStorage.setItem("ticket_code_value", var_condtion_method);
-    	console.log(localStorage.getItem('ticket_code_value'));
-
 		return localStorage.getItem('ticket_code_value');
 	});
 	if(localStorage.getItem('ticket_code_value')){
