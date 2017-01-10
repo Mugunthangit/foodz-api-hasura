90d74de7710e9d7e02872adbf587e6f836725cb9
devuxk <devakumar@crayond.com>
2017-01-10 17:32:52 +0530

    sign up api completed

--- components/https/sign_up_hasura_post.js
+++ components/https/sign_up_hasura_post.js
@@ -1,6 +1,5 @@
 var request = require('request');
 module.exports = function(req,res,type,url,head,body){
-	console.log(head)
 	request({
 		url: url,
 		method: type,
@@ -8,25 +7,28 @@ module.exports = function(req,res,type,url,head,body){
 		json: body
 	}, function(error, response, body){
 		if(error) {
-			console.log(error);
-			res.send(error)
+			res.send(response.statusCode,error)
 		} else {
-			request({
-				url: 'http://auth.hasura/admin/user/assign-role',
-				method: type,
-				headers: head,
-				json: {
-					"hasura_id": body.user.id,
-					"role": req.body.role
-				}
-			}, function(error, response, body){
-				if(error) {
-					console.log(error);
-				} else {
-					console.log(response.statusCode, body);
-					res.send(response.statusCode, body)
-				}
-			});
+			if (body.user) {
+				request({
+					url: 'http://auth.hasura/admin/user/assign-role',
+					method: type,
+					headers: head,
+					json: {
+						"hasura_id": body.user.id,
+						"role": req.body.role
+					}
+				}, function(error, response, body){
+					if(error) {
+						console.log(error);
+					} else {
+						res.send(response.statusCode, body)
+					}
+				});
+			}
+			else {
+				res.send(response.statusCode, body)
+			}
 		}
 	});
 }
