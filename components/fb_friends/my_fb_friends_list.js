7d4c4aafe6840f53acab1a30fcae2b0f2c42a0ec
Mugunthan <mugunthan@crayond.com>
2017-01-20 21:53:22 +0530

    final_before_stage

--- components/fb_friends/my_fb_friends_list.js
+++ components/fb_friends/my_fb_friends_list.js
@@ -1,18 +1,19 @@
 var request = require('request');
 var myParser = require("body-parser");
+var restaurant_sponsors = require('./fb_friends_restaurant_sponsors');
+
 
 module.exports = function(app){
 	app.post("/my_fb_friends", function(req, res){ 
-	var data = []
-	var fb_friends_data =  req.body.fb_user_friend;
-	for (i=0; i<fb_friends_data.length; i++){	
-		var obj = fb_friends_data[i];
-		console.log(obj.fb_friend_name)
+	var callback_obj = {};
+	restaurant_sponsors(app,callback_obj,req.body.tbl_restaurantsunique_id, req.body.tbl_user_profileunique_id);
+	setTimeout(function() {
+	if (callback_obj.restaurant_sponsor_value != null) { 
+		var fb_friends_data =  req.body.fb_user_friend;
+		fb_friends_data.forEach(function (fb_user){
+
+		console.log(fb_user.fb_friend_name)
 		console.log("===================")
-		// console.log(obj1)
-		// console.log("===================")
-		// console.log(obj.friends_facebook_id);
-		console.log(i);
         request({
         	url: 'https://data.foodz.fr/v1/query',
 			method: 'POST',
@@ -23,7 +24,7 @@ module.exports = function(app){
 			    "table" : "tbl_user_facebook_friends",
 			    "columns": ["*.*"],
 			    "where":{
-			    	"fb_friend_name":obj.fb_friend_name
+			    	"fb_friend_name":fb_user.fb_friend_name
 			    }
 			  }
 			}
@@ -31,85 +32,299 @@ module.exports = function(app){
 			if(error) {
 				console.log(error);
 			} else {
-				console.log(response.body.length);
+				console.log(response.statusCode, body);
 				console.log("It works")
-				// res.send(response.statusCode, body)
-			if (response.body.length == 0) { 
-			request({
-			url: 'https://data.foodz.fr/v1/query',
-			method: 'POST',
-			headers: {'Content-Type':'application/json','Authorization':'Bearer 5a8lqgvms1un9dlmfsvhgt2m56dhuc3m'},
-			json: {
-			  "type" : "insert",
-			  "args" : {
-			    "table" : "tbl_user_facebook_friends",
-			    "returning": [
-						"friends_facebook_id",
-						"fb_friend_name",
-						"fb_profile_image_url",
-						"tbl_user_profileunique_id"					],
-			    "objects":[
-			    {
-			    	"friends_facebook_id":obj.friends_facebook_id,
-			    	"fb_friend_name": obj.fb_friend_name,
-			    	"fb_profile_image_url":obj.fb_profile_image_url,
-			    	"tbl_user_profileunique_id":req.body.tbl_user_profileunique_id			    
-			    }
-			    ]
-			  }
+				console.log("success")
+				console.log(callback_obj.restaurant_sponsor_value)
+				if (body == 0) { 
+						request({
+						url: 'https://data.foodz.fr/v1/query',
+						method: 'POST',
+						headers: {'Content-Type':'application/json','Authorization':'Bearer 5a8lqgvms1un9dlmfsvhgt2m56dhuc3m'},
+						json: {
+						  "type" : "insert",
+						  "args" : {
+						    "table" : "tbl_user_facebook_friends",
+						    "returning": [
+									"friends_facebook_id",
+									"fb_friend_name",
+									"fb_profile_image_url",
+									"tbl_user_profileunique_id"				
+									],
+						    "objects":[
+						    {
+						    	"friends_facebook_id":fb_user.friends_facebook_id,
+						    	"fb_friend_name": fb_user.fb_friend_name,
+						    	"fb_profile_image_url":fb_user.fb_profile_image_url,
+						    	"tbl_user_profileunique_id":req.body.tbl_user_profileunique_id			    
+						    }
+						    ]
+						  }
+						}
+						},
+						function(error, response, body){
+						if(error) {
+							console.log(error);
+						} else {
+							console.log(body);
+							console.log("It 1111111111111111111111111111111111111111111")
+							request({
+							url: 'https://data.foodz.fr/v1/query',
+							method: 'POST',
+							headers: {'Content-Type':'application/json','Authorization':'Bearer 5a8lqgvms1un9dlmfsvhgt2m56dhuc3m'},
+									json: {
+									  "type" : "select",
+									  "args" : {
+									    "table" : "tbl_user_facebook_friends",
+									    "columns": ["*"],
+									    "where": {"fb_friend_name":fb_user.fb_friend_name }
+									  }
+									}
+								}, function(error, response, body){
+									if(error) {
+										console.log(error);
+									} else {
+										console.log("success")
+										fb_user["is_check"] = true
+										console.log("======================================================================")
+										console.log(fb_user)
+										// res.send(response.statusCode, fb_obj)
+								}
+								});
+							// res.send(response.statusCode, body)
+						}
+						});
+						}
+						else{
+						request({
+						url: 'https://data.foodz.fr/v1/query',
+						method: 'POST',
+						headers: {'Content-Type':'application/json','Authorization':'Bearer 5a8lqgvms1un9dlmfsvhgt2m56dhuc3m'},
+						json: {
+						  "type" : "update",
+						  "args" : {
+						    "table" : "tbl_user_facebook_friends",
+						    "returning": [
+									"friends_facebook_id",
+									"fb_friend_name",
+									"fb_profile_image_url",
+									"tbl_user_profileunique_id"
+								],
+								"$set": {
+								"friends_facebook_id":fb_user.friends_facebook_id,
+								"fb_friend_name":fb_user.fb_friend_name,
+								"fb_profile_image_url":fb_user.fb_profile_image_url
+							},
+							"where":{
+								"fb_friend_name":fb_user.fb_friend_name
+							}
+						  }
+						}
+					}, function(error, response, body){
+						if(error) {
+							console.log(error);
+						} else {
+							console.log(body);
+							console.log("It sucks")
+							request({
+							url: 'https://data.foodz.fr/v1/query',
+							method: 'POST',
+							headers: {'Content-Type':'application/json','Authorization':'Bearer 5a8lqgvms1un9dlmfsvhgt2m56dhuc3m'},
+									json: {
+									  "type" : "select",
+									  "args" : {
+									    "table" : "tbl_user_facebook_friends",
+									    "columns": ["*"],
+									    "where": {"fb_friend_name":fb_user.fb_friend_name }
+									  }
+									}
+								}, function(error, response, body){
+									if(error) {
+										console.log(error);
+									} else {
+										console.log("success")
+										fb_user["is_check"] = true
+										console.log("======================================================================")
+										console.log(fb_user)
+								}
+								});
+
+						}
+					});
+					}
+				
+				
+
+			
 			}
-			},
-			function(error, response, body){
-			if(error) {
-				console.log(error);
-			} else {
-				console.log(response.body);
-				console.log("It Passes")
-				// res.send(response.statusCode, body)
-			}
-			});
+		    
+       	 })
+		})
+		setTimeout(function() {
+				    res.send(fb_friends_data);
+				    console.log('after response','---------------------------------------');
+				    return true;
+				}, 2000);
 			}
 			else{
-			request({
-			url: 'https://data.foodz.fr/v1/query',
+				var fb_friends_data =  req.body.fb_user_friend;
+		fb_friends_data.forEach(function (fb_user){
+
+		console.log(fb_user.fb_friend_name)
+		console.log("===================")
+        request({
+        	url: 'https://data.foodz.fr/v1/query',
 			method: 'POST',
 			headers: {'Content-Type':'application/json','Authorization':'Bearer 5a8lqgvms1un9dlmfsvhgt2m56dhuc3m'},
 			json: {
-			  "type" : "update",
+			  "type" : "select",
 			  "args" : {
 			    "table" : "tbl_user_facebook_friends",
-			    "returning": [
-						"friends_facebook_id",
-						"fb_friend_name",
-						"fb_profile_image_url",
-						"tbl_user_profileunique_id"
-					],
-					"$set": {
-					"friends_facebook_id":obj.friends_facebook_id,
-					"fb_friend_name":obj.fb_friend_name,
-					"fb_profile_image_url":obj.fb_profile_image_url
-				},
-				"where":{
-					"fb_friend_name":obj.fb_friend_name
-				}
+			    "columns": ["*.*"],
+			    "where":{
+			    	"fb_friend_name":fb_user.fb_friend_name
+			    }
 			  }
 			}
 		}, function(error, response, body){
 			if(error) {
 				console.log(error);
 			} else {
-				console.log(response.body);
-				console.log("It sucks")
-				var new_values = response.body
-				new_values.forEach(function (vals){
-					console.log(vals)
-				})
-				// res.send(response.statusCode, body)
+				console.log(response.statusCode, body);
+				console.log("It works")
+				console.log("success")
+				console.log(callback_obj.restaurant_sponsor_value)
+				if (body == 0) { 
+						request({
+						url: 'https://data.foodz.fr/v1/query',
+						method: 'POST',
+						headers: {'Content-Type':'application/json','Authorization':'Bearer 5a8lqgvms1un9dlmfsvhgt2m56dhuc3m'},
+						json: {
+						  "type" : "insert",
+						  "args" : {
+						    "table" : "tbl_user_facebook_friends",
+						    "returning": [
+									"friends_facebook_id",
+									"fb_friend_name",
+									"fb_profile_image_url",
+									"tbl_user_profileunique_id"				
+									],
+						    "objects":[
+						    {
+						    	"friends_facebook_id":fb_user.friends_facebook_id,
+						    	"fb_friend_name": fb_user.fb_friend_name,
+						    	"fb_profile_image_url":fb_user.fb_profile_image_url,
+						    	"tbl_user_profileunique_id":req.body.tbl_user_profileunique_id			    
+						    }
+						    ]
+						  }
+						}
+						},
+						function(error, response, body){
+						if(error) {
+							console.log(error);
+						} else {
+							console.log(body);
+							console.log("It 1111111111111111111111111111111111111111111")
+							request({
+							url: 'https://data.foodz.fr/v1/query',
+							method: 'POST',
+							headers: {'Content-Type':'application/json','Authorization':'Bearer 5a8lqgvms1un9dlmfsvhgt2m56dhuc3m'},
+									json: {
+									  "type" : "select",
+									  "args" : {
+									    "table" : "tbl_user_facebook_friends",
+									    "columns": ["*"],
+									    "where": {"fb_friend_name":fb_user.fb_friend_name }
+									  }
+									}
+								}, function(error, response, body){
+									if(error) {
+										console.log(error);
+									} else {
+										console.log("success")
+										fb_user["is_check"] = false
+										console.log("======================================================================")
+										console.log(fb_user)
+										// res.send(response.statusCode, fb_obj)
+								}
+								});
+							// res.send(response.statusCode, body)
+						}
+						});
+						}
+						else{
+						request({
+						url: 'https://data.foodz.fr/v1/query',
+						method: 'POST',
+						headers: {'Content-Type':'application/json','Authorization':'Bearer 5a8lqgvms1un9dlmfsvhgt2m56dhuc3m'},
+						json: {
+						  "type" : "update",
+						  "args" : {
+						    "table" : "tbl_user_facebook_friends",
+						    "returning": [
+									"friends_facebook_id",
+									"fb_friend_name",
+									"fb_profile_image_url",
+									"tbl_user_profileunique_id"
+								],
+								"$set": {
+								"friends_facebook_id":fb_user.friends_facebook_id,
+								"fb_friend_name":fb_user.fb_friend_name,
+								"fb_profile_image_url":fb_user.fb_profile_image_url
+							},
+							"where":{
+								"fb_friend_name":fb_user.fb_friend_name
+							}
+						  }
+						}
+					}, function(error, response, body){
+						if(error) {
+							console.log(error);
+						} else {
+							console.log(body);
+							console.log("It sucks")
+							request({
+							url: 'https://data.foodz.fr/v1/query',
+							method: 'POST',
+							headers: {'Content-Type':'application/json','Authorization':'Bearer 5a8lqgvms1un9dlmfsvhgt2m56dhuc3m'},
+									json: {
+									  "type" : "select",
+									  "args" : {
+									    "table" : "tbl_user_facebook_friends",
+									    "columns": ["*"],
+									    "where": {"fb_friend_name":fb_user.fb_friend_name }
+									  }
+									}
+								}, function(error, response, body){
+									if(error) {
+										console.log(error);
+									} else {
+										console.log("success")
+										fb_user["is_check"] = false
+										console.log("======================================================================")
+										console.log(fb_user)
+								}
+								});
+
+						}
+					});
+					}
+				
+				
+
+			
 			}
-		});
-		}
-		}
-        })
-	}
+		    
+       	 })
+		})
+		setTimeout(function() {
+				    res.send(fb_friends_data);
+				    console.log('after response','---------------------------------------');
+				    return true;
+				}, 2000);
+			}
+		}, 3000)
+
 	});
 };
