173248124e6a182d90ab0bbb47fc9be3d1f99cd1
devuxk <devakumar@crayond.com>
2017-01-22 22:55:09 +0530

    Get master data for trendscore

--- components/https/bookmark_value.js
+++ components/https/bookmark_value.js
@@ -29,8 +29,6 @@ request({url: 'http://data.hasura/v1/template/tbl_user_favorite_list?tbl_user_pr
 
     	var bookmarked_condition = isInArray(restaurant_unique_id,new_array);
 		// var bookmarked_condition = new_array.includes(restaurant_unique_id);
-		console.log('^^^^^^^^^^^^^^')
-		console.log(bookmarked_condition);
 		callback(bookmarked_condition)
 	}
 });	
@@ -39,8 +37,6 @@ request({url: 'http://data.hasura/v1/template/tbl_user_favorite_list?tbl_user_pr
 var myCallback = function(data) {
 	console.log('got data: '+data);
 	x['is_bookmarked'] = data;
-	console.log(x['is_bookmarked'],"===============")
-	console.log(x);
 };
 req_var(myCallback);
    
